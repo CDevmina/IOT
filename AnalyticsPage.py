943f23808139c5049b9ffb8926af7157029c6f23
Killer3001 <chamathdewmina25@gmail.com>
2024-03-21 14:22:26 +0530

    analytics update commit (temp)

--- AnalyticsPage.py
+++ AnalyticsPage.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QHBoxLayout, QPushButton
+import sqlite3
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QHBoxLayout, QPushButton, QLabel
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from DB_Scripts.vehicle_anaylitics import totalcount_graph, average_speed_graph, reported_vehicles_graph , busiest_entrance_exit_graph
@@ -13,33 +15,62 @@ class AnalyticsWindow(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
+        # Create labels for the filters
+        self.label1 = QLabel('Graph 1:')
+        self.label2 = QLabel('Graph 2:')
+        self.label3 = QLabel('Graph 3:')
+        self.label4 = QLabel('Graph 4:')
+
         # Create a combo box for the filter
-        self.filter_combo = QComboBox()
-        self.filter_combo.addItems(['Hourly', 'Daily', 'Monthly', 'Yearly'])
-        self.filter_combo.currentTextChanged.connect(self.update_graph)
+        self.filter_combo1 = QComboBox()
+        self.filter_combo1.addItems(['Hourly', 'Daily', 'Monthly', 'Yearly'])
+        self.filter_combo1.currentTextChanged.connect(self.update_filter_options)
 
-        # Create three more combo boxes
-        self.combo2 = QComboBox()
-        self.combo2.addItems(['Hourly', 'Daily', 'Monthly', 'Yearly'])
-        self.combo2.currentTextChanged.connect(self.update_graph)
+        self.filter_combo2 = QComboBox()
+        self.filter_combo2.currentTextChanged.connect(self.update_graph)
 
-        self.combo3 = QComboBox()
-        self.combo3.addItems(['Hourly', 'Daily', 'Monthly', 'Yearly'])
-        self.combo3.currentTextChanged.connect(self.update_graph)
+        # Create three more pairs of combo boxes
+        self.combo2_1 = QComboBox()
+        self.combo2_1.addItems(['Hourly', 'Daily', 'Monthly', 'Yearly'])
+        self.combo2_1.currentTextChanged.connect(self.update_filter_options)
 
-        self.combo4 = QComboBox()
-        self.combo4.addItems(['Entrance', 'Exit'])
-        self.combo4.currentTextChanged.connect(self.update_graph)
+        self.combo2_2 = QComboBox()
+        self.combo2_2.currentTextChanged.connect(self.update_graph)
+
+        self.combo3_1 = QComboBox()
+        self.combo3_1.addItems(['Hourly', 'Daily', 'Monthly', 'Yearly'])
+        self.combo3_1.currentTextChanged.connect(self.update_filter_options)
+
+        self.combo3_2 = QComboBox()
+        self.combo3_2.currentTextChanged.connect(self.update_graph)
+
+        self.combo4_1 = QComboBox()
+        self.combo4_1.addItems(self.get_all_entrances_exits())
+        self.combo4_1.currentTextChanged.connect(self.update_filter_options)
+
+        self.combo4_2 = QComboBox()
+        self.combo4_2.currentTextChanged.connect(self.update_graph)
+
 
         # Create a layout for the first two combo boxes
         self.layout1 = QHBoxLayout()
-        self.layout1.addWidget(self.filter_combo)
-        self.layout1.addWidget(self.combo2)
+        self.layout1.setSpacing(10)  # Reduce the spacing
+        self.layout1.addWidget(self.label1)
+        self.layout1.addWidget(self.filter_combo1)
+        self.layout1.addWidget(self.filter_combo2)
+        self.layout1.addWidget(self.label2)
+        self.layout1.addWidget(self.combo2_1)
+        self.layout1.addWidget(self.combo2_2)
 
         # Create a layout for the last two combo boxes
         self.layout2 = QHBoxLayout()
-        self.layout2.addWidget(self.combo3)
-        self.layout2.addWidget(self.combo4)
+        self.layout2.setSpacing(10)  # Reduce the spacing
+        self.layout2.addWidget(self.label3)
+        self.layout2.addWidget(self.combo3_1)
+        self.layout2.addWidget(self.combo3_2)
+        self.layout2.addWidget(self.label4)
+        self.layout2.addWidget(self.combo4_1)
+        self.layout2.addWidget(self.combo4_2)
 
         # Add the layouts to the main layout
         self.layout.addLayout(self.layout1)
@@ -55,8 +86,77 @@ class AnalyticsWindow(QWidget):
         self.back_button.clicked.connect(self.go_back)
         self.layout.addWidget(self.back_button)
 
-        # Update the graph
+        # Update the graph with default filter values
+        self.update_filter_options()
         self.update_graph()
+
+    def update_filter_options(self):
+        sender = self.sender()
+
+        if sender is None:
+            sender = self.filter_combo1
+            target = self.filter_combo2
+        else:
+            if sender == self.filter_combo1:
+                target = self.filter_combo2
+            elif sender == self.combo2_1:
+                target = self.combo2_2
+            elif sender == self.combo3_1:
+                target = self.combo3_2
+            elif sender == self.combo4_1:
+                target = self.combo4_2
+            else:
+                return
+
+        filter1 = sender.currentText()
+
+        target.clear()
+
+        if sender == self.combo4_1:
+            target.addItems(self.get_all_days())
+        else:
+            if filter1 == 'Hourly':
+                target.addItems(self.get_all_days())
+            elif filter1 == 'Daily':
+                target.addItems(self.get_all_months())
+            elif filter1 == 'Monthly':
+                target.addItems(self.get_all_years())
+            elif filter1 == 'Yearly':
+                target.addItem('N/A')
+
+
+    def get_all_entrances_exits(self):
+        conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT entrance FROM vehicles UNION SELECT DISTINCT exit FROM vehicles ORDER BY entrance')
+        entrances_exits = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return entrances_exits
+
+
+    def get_all_days(self):
+        conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT strftime("%Y-%m-%d", time_entered) as Day FROM vehicles ORDER BY Day')
+        days = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return days
+
+    def get_all_months(self):
+        conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT strftime("%Y-%m", time_entered) as Month FROM vehicles ORDER BY Month')
+        months = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return months
+
+    def get_all_years(self):
+        conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT strftime("%Y", time_entered) as Year FROM vehicles ORDER BY Year')
+        years = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return years
 
     def update_graph(self):
         # Clear the figure
@@ -64,16 +164,16 @@ class AnalyticsWindow(QWidget):
 
         # Create the graphs
         ax1 = self.figure.add_subplot(2, 2, 1)
-        totalcount_graph(ax1, self.filter_combo.currentText())
+        totalcount_graph(ax1, self.filter_combo1.currentText(), self.filter_combo2.currentText())
 
         ax2 = self.figure.add_subplot(2, 2, 2)
-        average_speed_graph(ax2, self.combo2.currentText())
+        average_speed_graph(ax2, self.combo2_1.currentText(), self.combo2_2.currentText())
 
         ax3 = self.figure.add_subplot(2, 2, 3)
-        reported_vehicles_graph(ax3, self.combo3.currentText())
+        reported_vehicles_graph(ax3, self.combo3_1.currentText(), self.combo3_2.currentText())
 
         ax4 = self.figure.add_subplot(2, 2, 4)
-        busiest_entrance_exit_graph(ax4, self.combo4.currentText())
+        #busiest_entrance_exit_graph(ax4, self.combo4_1.currentText(), self.combo4_2.currentText())
 
         # Adjust the spacing between the subplots
         self.figure.subplots_adjust(hspace=0.5, wspace=0.3)
@@ -90,3 +190,13 @@ class AnalyticsWindow(QWidget):
         # Open the AdminHome page
         self.admin_home = AdminHome()
         self.admin_home.show()
+
+
+if __name__ == '__main__':
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    window = AnalyticsWindow()
+    window.show()
+    sys.exit(app.exec_())
--- DB_Scripts/vehicle_anaylitics.py
+++ DB_Scripts/vehicle_anaylitics.py
@@ -2,40 +2,36 @@ import sqlite3
 from datetime import datetime, timedelta
 
 
-def totalcount_graph(ax, filter):
+def totalcount_graph(ax, filter1, filter2):
     # Query database
     conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
     cursor = conn.cursor()
 
-    if filter == 'Hourly':
-        cursor.execute('SELECT strftime("%H - ", time_entered) || strftime("%H", datetime(time_entered, "+1 hour")) as Hour, vehicle_class, COUNT(*) as Count FROM vehicles WHERE time_entered > datetime("now", "-24 hours") GROUP BY strftime("%H", time_entered), vehicle_class')
-    elif filter == 'Daily':
-        cursor.execute('SELECT strftime("%m/%d", time_entered) as Day, vehicle_class, COUNT(*) as Count FROM vehicles WHERE time_entered > datetime("now", "-30 days") GROUP BY strftime("%m/%d", time_entered), vehicle_class')
-    elif filter == 'Monthly':
-        cursor.execute('SELECT strftime("%m-%Y", time_entered) as Month, vehicle_class, COUNT(*) as Count FROM vehicles WHERE strftime("%Y", time_entered) = strftime("%Y", "now") GROUP BY strftime("%m", time_entered), vehicle_class')
-    elif filter == 'Yearly':
-        cursor.execute('SELECT strftime("%Y", time_entered) as Year, vehicle_class, COUNT(*) as Count FROM vehicles GROUP BY strftime("%Y", time_entered), vehicle_class')
+    if filter1 == 'Hourly':
+        cursor.execute('SELECT strftime("%H:00", time_entered) as Hour, COUNT(*) as Count FROM vehicles WHERE strftime("%Y-%m-%d", time_entered) = ? GROUP BY strftime("%H", time_entered)', (filter2,))
+    elif filter1 == 'Daily':
+        cursor.execute('SELECT strftime("%m-%d", time_entered) as Day, COUNT(*) as Count FROM vehicles WHERE strftime("%Y-%m", time_entered) = ? GROUP BY strftime("%d", time_entered)', (filter2,))
+    elif filter1 == 'Monthly':
+        cursor.execute('SELECT strftime("%Y-%m", time_entered) as Month, COUNT(*) as Count FROM vehicles WHERE strftime("%Y", time_entered) = ? GROUP BY strftime("%Y-%m", time_entered)', (filter2,))
+    elif filter1 == 'Yearly':
+        cursor.execute('SELECT strftime("%Y", time_entered) as Year, COUNT(*) as Count FROM vehicles GROUP BY strftime("%Y", time_entered)')
     else:
-        raise ValueError(f"Invalid filter value: {filter}")
+        raise ValueError(f"Invalid filter value: {filter1}")
 
     data = cursor.fetchall()
     conn.close()
 
-    # Check if data contains at least three elements
-    if not data or len(data[0]) < 3:
+    # Check if data contains at least two elements
+    if not data or len(data[0]) < 2:
         print("No data or not enough columns returned from the SQL query.")
         return
 
     # Process data
     x = [row[0] for row in data]
-    y = [row[2] for row in data]
-    hue = [row[1] for row in data]
+    y = [int(row[1]) for row in data]  # Convert count to integer
 
     # Create a bar graph
-    for vehicle_type in set(hue):
-        y_filtered = [count if type == vehicle_type else 0 for count, type in zip(y, hue)]
-        ax.bar(x, y_filtered, label=vehicle_type)
-
+    ax.bar(x, y)
     ax.set_xlabel('Time')
     ax.set_ylabel('Number of Vehicles')
     ax.set_title('Total Count of Vehicles That Entered the Expressway')
@@ -44,28 +40,26 @@ def totalcount_graph(ax, filter):
     ax.set_xticks(range(len(x)))
     ax.set_xticklabels(x, rotation=90, fontsize=6)
 
-    # Add a legend
-    ax.legend()
-
     return data
 
-
-def average_speed_graph(ax, filter):
-    # Connect to the SQLite database
+def average_speed_graph(ax, filter1, filter2):
+    # Query database
     conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
     cursor = conn.cursor()
 
-    # Execute a SQL query to fetch the average speed of vehicles
-    if filter == 'Hourly':
-        cursor.execute('SELECT strftime("%H - ", time_entered) || strftime("%H", datetime(time_entered, "+1 hour")) as Hour, AVG(CAST(REPLACE(average_speed, " Km/h", "") AS FLOAT)) as AverageSpeed FROM vehicles WHERE time_entered > datetime("now", "-24 hours") GROUP BY strftime("%H", time_entered)')
-    elif filter == 'Daily':
-        cursor.execute('SELECT strftime("%m/%d", time_entered) as Day, AVG(CAST(REPLACE(average_speed, " Km/h", "") AS FLOAT)) as AverageSpeed FROM vehicles WHERE time_entered > datetime("now", "-30 days") GROUP BY strftime("%m/%d", time_entered)')
-    elif filter == 'Monthly':
-        cursor.execute('SELECT strftime("%m-%Y", time_entered) as Month, AVG(CAST(REPLACE(average_speed, " Km/h", "") AS FLOAT)) as AverageSpeed FROM vehicles WHERE strftime("%Y", time_entered) = strftime("%Y", "now") GROUP BY strftime("%m", time_entered)')
-    elif filter == 'Yearly':
-        cursor.execute('SELECT strftime("%Y", time_entered) as Year, AVG(CAST(REPLACE(average_speed, " Km/h", "") AS FLOAT)) as AverageSpeed FROM vehicles GROUP BY strftime("%Y", time_entered)')
+    # Determine the start time for the last 24 hours
+    start_time = datetime.now() - timedelta(hours=24)
+
+    if filter1 == 'Hourly':
+        cursor.execute('SELECT strftime("%H:00", time_entered) as Hour, AVG(average_speed) as AverageSpeed FROM vehicles WHERE time_entered > ? AND strftime("%Y-%m-%d", time_entered) = ? GROUP BY strftime("%H", time_entered)', (start_time, filter2))
+    elif filter1 == 'Daily':
+        cursor.execute('SELECT strftime("%Y-%m-%d", time_entered) as Day, AVG(average_speed) as AverageSpeed FROM vehicles WHERE time_entered > ? AND strftime("%Y-%m", time_entered) = ? GROUP BY strftime("%Y-%m-%d", time_entered)', (start_time, filter2))
+    elif filter1 == 'Monthly':
+        cursor.execute('SELECT strftime("%Y-%m", time_entered) as Month, AVG(average_speed) as AverageSpeed FROM vehicles WHERE strftime("%Y", time_entered) = strftime("%Y", "now") AND strftime("%Y", time_entered) = ? GROUP BY strftime("%Y-%m", time_entered)', (filter2,))
+    elif filter1 == 'Yearly':
+        cursor.execute('SELECT strftime("%Y", time_entered) as Year, AVG(average_speed) as AverageSpeed FROM vehicles WHERE strftime("%Y", time_entered) = ? GROUP BY strftime("%Y", time_entered)', (filter2,))
     else:
-        raise ValueError(f"Invalid filter value: {filter}")
+        raise ValueError(f"Invalid filter value: {filter1}")
 
     data = cursor.fetchall()
     conn.close()
@@ -82,7 +76,7 @@ def average_speed_graph(ax, filter):
     # Create a line graph
     ax.plot(x, y)
     ax.set_xlabel('Time')
-    ax.set_ylabel('Average Speed')
+    ax.set_ylabel('Average Speed (km/h)')
     ax.set_title('Average Speed of Vehicles That Entered the Expressway')
 
     # Set the x-ticks and rotate the labels and reduce their font size
@@ -91,63 +85,23 @@ def average_speed_graph(ax, filter):
 
     return data
 
-def reported_vehicles_graph(ax, filter):
+def reported_vehicles_graph(ax, filter, filter2):
     # Connect to the SQLite database
     conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
     cursor = conn.cursor()
+
+    # Determine the start time for the last 24 hours
+    start_time = datetime.now() - timedelta(hours=24)
 
     # Execute a SQL query to fetch the count of reported vehicles
     if filter == 'Hourly':
-        cursor.execute('SELECT strftime("%H - ", time_entered) || strftime("%H", datetime(time_entered, "+1 hour")) as Hour, report_status, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" AND time_entered > datetime("now", "-24 hours") GROUP BY strftime("%H", time_entered), report_status')
+        cursor.execute('SELECT strftime("%H:00", time_entered) as Hour, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" AND time_entered > ? GROUP BY strftime("%H", time_entered)', (start_time,))
     elif filter == 'Daily':
-        cursor.execute('SELECT strftime("%m/%d", time_entered) as Day, report_status, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" AND time_entered > datetime("now", "-30 days") GROUP BY strftime("%m/%d", time_entered), report_status')
+        cursor.execute('SELECT strftime("%Y-%m-%d", time_entered) as Day, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" AND time_entered > ? GROUP BY strftime("%Y-%m-%d", time_entered)', (start_time,))
     elif filter == 'Monthly':
-        cursor.execute('SELECT strftime("%m-%Y", time_entered) as Month, report_status, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" AND strftime("%Y", time_entered) = strftime("%Y", "now") GROUP BY strftime("%m", time_entered), report_status')
+        cursor.execute('SELECT strftime("%Y-%m", time_entered) as Month, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" AND strftime("%Y", time_entered) = strftime("%Y", "now") GROUP BY strftime("%Y-%m", time_entered)')
     elif filter == 'Yearly':
-        cursor.execute('SELECT strftime("%Y", time_entered) as Year, report_status, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" GROUP BY strftime("%Y", time_entered), report_status')
-    else:
-        raise ValueError(f"Invalid filter value: {filter}")
-
-    data = cursor.fetchall()
-    conn.close()
-
-    # Check if data contains at least three elements
-    if not data or len(data[0]) < 3:
-        print("No data or not enough columns returned from the SQL query.")
-        return
-
-    # Process data
-    x = [row[0] for row in data]
-    y = [row[2] for row in data]
-    hue = [row[1] for row in data]
-
-    for report_type in set(hue):
-        y_filtered = [count if type == report_type else 0 for count, type in zip(y, hue)]
-        ax.bar(x, y_filtered, label=report_type)
-
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Count of Reported Vehicles')
-    ax.set_title('Count of Reported Vehicles That Entered the Expressway')
-
-    # Set the x-ticks and rotate the labels and reduce their font size
-    ax.set_xticks(range(len(x)))
-    ax.set_xticklabels(x, rotation=90, fontsize=6)
-
-    # Add a legend
-    ax.legend()
-
-    return data
-
-def busiest_entrance_exit_graph(ax, filter):
-    # Connect to the SQLite database
-    conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
-    cursor = conn.cursor()
-
-    # Execute a SQL query to fetch the count of vehicles by entrance or exit
-    if filter == 'Entrance':
-        cursor.execute('SELECT entrance, COUNT(*) as Count FROM vehicles WHERE time_entered > datetime("now", "-12 months") GROUP BY entrance')
-    elif filter == 'Exit':
-        cursor.execute('SELECT exit, COUNT(*) as Count FROM vehicles WHERE time_entered > datetime("now", "-12 months") GROUP BY exit')
+        cursor.execute('SELECT strftime("%Y", time_entered) as Year, COUNT(*) as Count FROM vehicles WHERE report_status != "Normal" GROUP BY strftime("%Y", time_entered)')
     else:
         raise ValueError(f"Invalid filter value: {filter}")
 
@@ -165,9 +119,50 @@ def busiest_entrance_exit_graph(ax, filter):
 
     # Create a bar graph
     ax.bar(x, y)
-    ax.set_xlabel(filter)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Count of Reported Vehicles')
+    ax.set_title('Count of Reported Vehicles That Entered the Expressway')
+
+    # Set the x-ticks and rotate the labels and reduce their font size
+    ax.set_xticks(range(len(x)))
+    ax.set_xticklabels(x, rotation=90, fontsize=6)
+
+    return data
+
+def busiest_entrance_exit_graph(ax, filter1, filter2):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('D:\IOT\Database/vehicle_database.db')
+    cursor = conn.cursor()
+
+    # Execute a SQL query to fetch the average count of vehicles by entrance or exit
+    if filter2 == 'Hourly':
+        cursor.execute('SELECT strftime("%H:00", time_entered) as Hour, COUNT(*) as Count FROM vehicles WHERE {} = ? GROUP BY strftime("%H", time_entered)'.format(filter1), (filter1,))
+    elif filter2 == 'Daily':
+        cursor.execute('SELECT strftime("%Y-%m-%d", time_entered) as Day, COUNT(*) as Count FROM vehicles WHERE {} = ? GROUP BY strftime("%Y-%m-%d", time_entered)'.format(filter1), (filter1,))
+    elif filter2 == 'Monthly':
+        cursor.execute('SELECT strftime("%Y-%m", time_entered) as Month, COUNT(*) as Count FROM vehicles WHERE {} = ? GROUP BY strftime("%Y-%m", time_entered)'.format(filter1), (filter1,))
+    elif filter2 == 'Yearly':
+        cursor.execute('SELECT strftime("%Y", time_entered) as Year, COUNT(*) as Count FROM vehicles WHERE {} = ? GROUP BY strftime("%Y", time_entered)'.format(filter1), (filter1,))
+    else:
+        raise ValueError(f"Invalid filter value: {filter2}")
+
+    data = cursor.fetchall()
+    conn.close()
+
+    # Check if data contains at least two elements
+    if not data or len(data[0]) < 2:
+        print("No data or not enough columns returned from the SQL query.")
+        return
+
+    # Process data
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    # Create a bar graph
+    ax.bar(x, y)
+    ax.set_xlabel(filter2)
     ax.set_ylabel('Number of Vehicles')
-    ax.set_title(f'Busiest {filter}s in the Last 12 Months')
+    ax.set_title(f'Average Number of Vehicles at {filter1} {filter2}')
 
     # Set the x-ticks and rotate the labels and reduce their font size
     ax.set_xticks(range(len(x)))
